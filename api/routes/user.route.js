6f5dd4fe6aeea40eaab8b2422dc4b35f84b20efa
Ammen1 <amenguda@gmail.com>
2024-02-20 23:11:10 +0300

    update user controller and route

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { signout } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.post("/signout", signout);
+
+export default router;
