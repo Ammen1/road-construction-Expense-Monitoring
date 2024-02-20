61f471065f0c6575b7ccf6c45d2107c303d71da0
Ammen1 <amenguda@gmail.com>
2024-02-20 21:08:12 +0300

    Run server on port 3000

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express, json());
+
+app.listen(3000, () => {
+  console.log("Server is runing on port 300");
+});
