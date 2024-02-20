f392432eb2e9b31f8332019d206fb3e1782bcff1
Ammen1 <amenguda@gmail.com>
2024-02-20 22:31:31 +0300

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
-import express, { json } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -13,9 +17,31 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
-app.use(express, json());
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is runing on port 300");
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
