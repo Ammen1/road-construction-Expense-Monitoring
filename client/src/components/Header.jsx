457474c992745ac2e07c9d635c55adcabe521b2a
Ammen1 <amenguda@gmail.com>
2024-02-20 19:02:24 +0300

    create stly and and index.css create header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
@@ -9,9 +10,9 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Re
+          RoadCon
         </span>
-        Blog
+        Monitor
       </Link>
       <form>
         <TextInput
@@ -28,6 +29,17 @@ export default function Header() {
           pill
         ></Button>
       </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
