eb4c9b01e00ec741fafc42470d4c4d3ae52787bb
Ammen1 <amenguda@gmail.com>
2024-02-21 10:05:06 +0300

    created income model

---
+++ api/models/income.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const incomeSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: "income",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Income", incomeSchema);
