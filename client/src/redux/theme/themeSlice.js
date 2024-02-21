f0fd08db54598342e712cb0baea5cbee1efe8b9c
Ammen1 <amenguda@gmail.com>
2024-02-21 09:36:48 +0300

    create themeslice for dark and light mode

--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -1,19 +1,19 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    theme: 'light',
+  theme: "dark",
 };
 
 const themeSlice = createSlice({
-    name: 'theme',
-    initialState,
-    reducers: {
-        toggleTheme: (state) => {
-            state.theme = state.theme === 'light' ? 'dark' : 'light';
-        },
-        }
+  name: "theme",
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
+  },
 });
 
-export const {toggleTheme} = themeSlice.actions;
+export const { toggleTheme } = themeSlice.actions;
 
 export default themeSlice.reducer;
