cd30de62693c785d1fad5c6100d49d29b8f4de8c
Ammen1 <amenguda@gmail.com>
2024-02-21 10:04:32 +0300

    create model for expense

---
+++ api/models/expense.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Expense", expenseSchema);
