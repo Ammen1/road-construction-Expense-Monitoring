53cb69dc009c2ee17f6f32db5006fe3be29ebb11
Ammen1 <amenguda@gmail.com>
2024-02-22 21:08:14 +0300

    create some fuctionality and ui dashboard

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashboardComp from "../components/DashboardComp";
+import Expenses from "../components/Expenses";
+
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+      {/* posts... */}
+      {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
+      {tab === "create-expense" && <Expenses />}
+    </div>
+  );
+}
