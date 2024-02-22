1694d982a7483c6b37b7bf03a0049860da9673f5
Ammen1 <amenguda@gmail.com>
2024-02-22 17:09:56 +0300

    Globleprovider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,17 @@ import "./index.css";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
+import { GlobalProvider } from "./context/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <GlobalProvider>
+          <App />
+        </GlobalProvider>
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 );
