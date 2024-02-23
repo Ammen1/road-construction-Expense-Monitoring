e9c52536e264087404cb7e0ebf14529b288e4001
Ammen1 <amenguda@gmail.com>
2024-02-23 09:18:23 +0300

    add card

--- client/src/components/Expenses.jsx
+++ client/src/components/Expenses.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from "react";
 import { useGlobalContext } from "../context/Context";
 import ExpenseForm from "./ExpenseForm";
+import { Card } from "flowbite-react";
 
 function Expenses() {
-  const { getExpenses } = useGlobalContext();
+  const { getExpenses, totalExpenses } = useGlobalContext();
 
   useEffect(() => {
     getExpenses();
   }, []);
   return (
-    <div>
-      <div className=" ">
-        <div className="">
-          <ExpenseForm />
-        </div>
+    <div className="  ">
+      <Card className=" text-center leading-7 bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900 ">
+        <h1>Expenses</h1>
+        <h2 className="">
+          Total Expense: <span>${totalExpenses()}</span>
+        </h2>
+      </Card>
+
+      <div className="">
+        <ExpenseForm />
       </div>
     </div>
   );
