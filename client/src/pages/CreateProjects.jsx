39c9aec4f140d89c8af826a51303cb80126d2d08
Ammen1 <amenguda@gmail.com>
2024-02-25 09:23:59 +0300

    update create project

--- client/src/pages/CreateProjects.jsx
+++ client/src/pages/CreateProjects.jsx
@@ -1,8 +1,10 @@
 // react/components/CreateProject.js
 
-import { Button, TextInput, Select } from "flowbite-react";
+import { Button, Label, TextInput, Select } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Datepicker } from "flowbite-react";
 
 export default function CreateProject() {
   const [formData, setFormData] = useState({
@@ -109,132 +111,159 @@ export default function CreateProject() {
   };
 
   return (
-    <div className="p-3 max-w-3xl mx-auto min-h-screen">
-      <h1 className="text-center text-3xl my-7 font-semibold">
-        Create a Project
-      </h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput
-            type="text"
-            placeholder="Project Name"
-            required
-            id="name"
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-          <TextInput
-            type="text"
-            placeholder="Project Description"
-            required
-            id="description"
-            value={formData.description}
-            onChange={(e) =>
-              setFormData({ ...formData, description: e.target.value })
-            }
-          />
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1 lg:mb-44 ">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Road Con
+            </span>
+            Monitor
+          </Link>
+          <p className="text-md mt-5">
+            This is a road construction Expense Monitoring System for a
+            contractor project. You can create project up with all fields .
+          </p>
         </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="name" />
+              <TextInput
+                type="text"
+                placeholder="name"
+                required
+                id="name"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+              <Label value="description" />
+              <TextInput
+                type="text"
+                placeholder="description"
+                required
+                id="description"
+                value={formData.description}
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
+              />
+              <Label value="budget" />
+              <TextInput
+                type="number"
+                placeholder="budget"
+                required
+                id="budget"
+                value={formData.budget}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    budget: parseFloat(e.target.value),
+                  })
+                }
+              />
+              <Label value="lacation" />
+              <TextInput
+                type="text"
+                placeholder="location"
+                required
+                id="lacation"
+                value={formData.location}
+                onChange={(e) =>
+                  setFormData({ ...formData, startDate: e.target.value })
+                }
+              />
+              <Label value="start date" />
+              <Datepicker
+                type="date"
+                placeholder="start date"
+                required
+                id="startDate"
+                value={formData.startDate}
+                onChange={(e) =>
+                  setFormData({ ...formData, startDate: e.target.value })
+                }
+              />
+              <Label value="To end date" />
+              <Datepicker
+                type="date"
+                placeholder="end date"
+                lang="am ET"
+                required
+                id="endDate"
+                value={formData.endDate}
+                onChange={(e) =>
+                  setFormData({ ...formData, endDate: e.target.value })
+                }
+              />
+            </div>
+            <Label value="manager" />
+            <Select
+              className="text-white"
+              label="select manager"
+              id="manager"
+              value={selectedManager}
+              onChange={(e) => setSelectedManager(e.target.value)}
+            >
+              <option value="" disabled>
+                Choose a Manager
+              </option>
+              {availableManagers.map((manager) => (
+                <option key={manager._id} value={manager._id}>
+                  {manager.username}
+                </option>
+              ))}
+            </Select>
+            {formData.tasks.map((task, index) => (
+              <div key={index} className="flex flex-col gap-4">
+                <Label value="task" />
+                <TextInput
+                  type="text"
+                  placeholder="task name"
+                  required
+                  value={task.name}
+                  onChange={(e) =>
+                    handleTaskChange(index, "name", e.target.value)
+                  }
+                />
+                <TextInput
+                  type="text"
+                  placeholder="description"
+                  value={task.description}
+                  onChange={(e) =>
+                    handleTaskChange(index, "description", e.target.value)
+                  }
+                />
+                <TextInput
+                  type="date"
+                  placeholder="due date"
+                  value={task.dueDate}
+                  onChange={(e) =>
+                    handleTaskChange(index, "dueDate", e.target.value)
+                  }
+                />
 
-        <TextInput
-          type="number"
-          placeholder="Budget"
-          required
-          id="budget"
-          value={formData.budget}
-          onChange={(e) =>
-            setFormData({ ...formData, budget: parseFloat(e.target.value) })
-          }
-        />
-        <TextInput
-          type="text"
-          placeholder="location"
-          required
-          id="lacation"
-          value={formData.location}
-          onChange={(e) =>
-            setFormData({ ...formData, startDate: e.target.value })
-          }
-        />
-        <TextInput
-          type="date"
-          placeholder="Start Date"
-          required
-          id="startDate"
-          value={formData.startDate}
-          onChange={(e) =>
-            setFormData({ ...formData, startDate: e.target.value })
-          }
-        />
+                <Button type="button" onClick={() => removeTask(index)}>
+                  Remove Task
+                </Button>
+              </div>
+            ))}
 
-        <TextInput
-          type="date"
-          placeholder="End Date"
-          required
-          id="endDate"
-          value={formData.endDate}
-          onChange={(e) =>
-            setFormData({ ...formData, endDate: e.target.value })
-          }
-        />
-
-        <Select
-          className="text-white"
-          label="Select Project Manager"
-          id="manager"
-          value={selectedManager}
-          onChange={(e) => setSelectedManager(e.target.value)}
-        >
-          <option value="" disabled>
-            Choose a Manager
-          </option>
-          {availableManagers.map((manager) => (
-            <option key={manager._id} value={manager._id}>
-              {manager.username}
-            </option>
-          ))}
-        </Select>
-        {formData.tasks.map((task, index) => (
-          <div key={index} className="flex flex-col gap-4">
-            <TextInput
-              type="text"
-              placeholder="Task Name"
-              required
-              value={task.name}
-              onChange={(e) => handleTaskChange(index, "name", e.target.value)}
-            />
-            <TextInput
-              type="text"
-              placeholder="Task Description"
-              value={task.description}
-              onChange={(e) =>
-                handleTaskChange(index, "description", e.target.value)
-              }
-            />
-            <TextInput
-              type="date"
-              placeholder="Task dueDate"
-              value={task.dueDate}
-              onChange={(e) =>
-                handleTaskChange(index, "dueDate", e.target.value)
-              }
-            />
-
-            <Button type="button" onClick={() => removeTask(index)}>
-              Remove Task
+            <Button type="button" onClick={addTask}>
+              Add Task
             </Button>
-          </div>
-        ))}
 
-        <Button type="button" onClick={addTask}>
-          Add Task
-        </Button>
+            <Button type="submit" gradientDuoTone="purpleToPink">
+              Publish
+            </Button>
 
-        <Button type="submit" gradientDuoTone="purpleToPink">
-          Publish
-        </Button>
-
-        {publishError && <p className="text-red-500">{publishError}</p>}
-      </form>
+            {publishError && <p className="text-red-500">{publishError}</p>}
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
