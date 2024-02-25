b78f41dcf430b3da902e9a2ee3bdb2ed1b88abc8
Ammen1 <amenguda@gmail.com>
2024-02-26 01:15:03 +0300

    update createproject page

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,34 +1,64 @@
-import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from 'firebase/storage';
-import { app } from '../firebase';
-import { useEffect, useState } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Alert, Button, Select, Label, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import "react-circular-progressbar/dist/styles.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CreateProject from "./CreateProjects";
+import { Link } from "react-router-dom";
 
 export default function UpdatePost() {
-  const [file, setFile] = useState(null);
-  const [imageUploadProgress, setImageUploadProgress] = useState(null);
-  const [imageUploadError, setImageUploadError] = useState(null);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    budget: {
+      materials: "",
+      labor: "",
+      equipment: "",
+      permits: "",
+      other: "",
+    },
+    // endDate: "",
+    location: "",
+    tasks: [
+      {
+        name: "",
+        description: "",
+        assignedTo: "",
+        dueDate: "",
+        status: "NotStarted",
+      },
+    ],
+  });
   const [publishError, setPublishError] = useState(null);
-  const { postId } = useParams();
+  const { projectId } = useParams();
 
   const navigate = useNavigate();
-    const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const [availableManagers, setAvailableManagers] = useState([]);
+  const [selectedManager, setSelectedManager] = useState(null);
+
+  useEffect(() => {
+    const getManagers = async () => {
+      try {
+        const res = await fetch("/api/user/getusers");
+        const data = await res.json();
+        if (res.ok) {
+          const managers = data.users.filter((user) => user.isManager);
+          setAvailableManagers(managers);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getManagers();
+  }, []);
 
   useEffect(() => {
     try {
       const fetchPost = async () => {
-        const res = await fetch(`/api/post/getposts?postId=${postId}`);
+        const res = await fetch(
+          `/api/project/getprojects?projectId=${projectId}`
+        );
         const data = await res.json();
         if (!res.ok) {
           console.log(data.message);
@@ -37,7 +67,7 @@ export default function UpdatePost() {
         }
         if (res.ok) {
           setPublishError(null);
-          setFormData(data.posts[0]);
+          setFormData(data.projects[0]);
         }
       };
 
@@ -45,54 +75,31 @@ export default function UpdatePost() {
     } catch (error) {
       console.log(error.message);
     }
-  }, [postId]);
+  }, [projectId]);
 
-  const handleUpdloadImage = async () => {
-    try {
-      if (!file) {
-        setImageUploadError('Please select an image');
-        return;
-      }
-      setImageUploadError(null);
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + '-' + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setImageUploadProgress(progress.toFixed(0));
-        },
-        (error) => {
-          setImageUploadError('Image upload failed');
-          setImageUploadProgress(null);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setImageUploadProgress(null);
-            setImageUploadError(null);
-            setFormData({ ...formData, image: downloadURL });
-          });
-        }
-      );
-    } catch (error) {
-      setImageUploadError('Image upload failed');
-      setImageUploadProgress(null);
-      console.log(error);
-    }
-  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const validTasks = formData.tasks.filter(
+        (task) => task.name.trim() !== ""
+      );
+      const projectDataWithManager = {
+        ...formData,
+        manager: selectedManager,
+        tasks: validTasks,
+      };
+
+      console.log("Project Data:", projectDataWithManager);
+      const res = await fetch(
+        `/api/project/updateproject/${formData._id}/${currentUser._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
       const data = await res.json();
       if (!res.ok) {
         setPublishError(data.message);
@@ -101,93 +108,218 @@ export default function UpdatePost() {
 
       if (res.ok) {
         setPublishError(null);
-        navigate(`/post/${data.slug}`);
+        navigate(`/dashboard?tab=projects`);
       }
     } catch (error) {
-      setPublishError('Something went wrong');
+      console.error(error);
     }
   };
+
+  const handleTaskChange = (index, field, value) => {
+    const updatedTasks = [...formData.tasks];
+    updatedTasks[index][field] = value;
+    setFormData({ ...formData, tasks: updatedTasks });
+  };
+
+  const addTask = () => {
+    setFormData({
+      ...formData,
+      tasks: [
+        ...formData.tasks,
+        {
+          name: "",
+          description: "",
+          status: "NotStarted",
+        },
+      ],
+    });
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...formData.tasks];
+    updatedTasks.splice(index, 1);
+    setFormData({ ...formData, tasks: updatedTasks });
+  };
+
   return (
-    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Update post</h1>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-          <TextInput
-            type='text'
-            placeholder='Title'
-            required
-            id='title'
-            className='flex-1'
-            onChange={(e) =>
-              setFormData({ ...formData, title: e.target.value })
-            }
-            value={formData.title}
-          />
-          <Select
-            onChange={(e) =>
-              setFormData({ ...formData, category: e.target.value })
-            }
-            value={formData.category}
-          >
-            <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
-          </Select>
+    <div className="min-h-screen mt-20 lg:mr-44">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1 lg:mb-44 ">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Road Con
+            </span>
+            Monitor
+          </Link>
+          <p className="text-md mt-5">
+            This is a road construction Expense Monitoring System for a
+            contractor project. You can create project up with all fields .
+          </p>
         </div>
-        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-          <FileInput
-            type='file'
-            accept='image/*'
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <Button
-            type='button'
-            gradientDuoTone='purpleToBlue'
-            size='sm'
-            outline
-            onClick={handleUpdloadImage}
-            disabled={imageUploadProgress}
-          >
-            {imageUploadProgress ? (
-              <div className='w-16 h-16'>
-                <CircularProgressbar
-                  value={imageUploadProgress}
-                  text={`${imageUploadProgress || 0}%`}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="name" />
+              <TextInput
+                type="text"
+                placeholder="name"
+                required
+                id="name"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+              <Label value="description" />
+              <TextInput
+                type="text"
+                placeholder="description"
+                required
+                id="description"
+                value={formData.description}
+                onChange={(e) =>
+                  setFormData({ ...formData, description: e.target.value })
+                }
+              />
+              {/* <Label value="lacation" />
+              <TextInput
+                type="text"
+                placeholder="location"
+                required
+                id="lacation"
+                value={formData.location}
+                onChange={(e) =>
+                  setFormData({ ...formData, location: e.target.value })
+                }
+              /> */}
+              <Label value="badget for equipment" />
+              <TextInput
+                type="number"
+                placeholder="equipment"
+                id="equipment"
+                value={formData.budget.equipment}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    budget: {
+                      ...formData.budget,
+                      equipment: parseFloat(e.target.value),
+                    },
+                  })
+                }
+              />
+              <Label value="budget for materials" />
+              <TextInput
+                type="number"
+                placeholder="materials"
+                required
+                id="materials"
+                value={formData.budget.materials}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    budget: {
+                      ...formData.budget,
+                      materials: parseFloat(e.target.value),
+                    },
+                  })
+                }
+              />
+              <Label value="budget for labor" />
+              <TextInput
+                type="number"
+                placeholder="Labor"
+                id="labor"
+                value={formData.budget.labor}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    budget: {
+                      ...formData.budget,
+                      labor: parseFloat(e.target.value),
+                    },
+                  })
+                }
+              />
+              {/* <Label value="To end date" />
+              <TextInput
+                type="date"
+                placeholder="end date"
+                required
+                id="endDate"
+                value={formData.endDate}
+                onChange={(e) =>
+                  setFormData({ ...formData, endDate: e.target.value })
+                }
+              /> */}
+            </div>
+            <Label value="manager" />
+            <Select
+              className="text-white"
+              label="select manager"
+              id="manager"
+              value={selectedManager}
+              onChange={(e) => setSelectedManager(e.target.value)}
+            >
+              <option value="" disabled>
+                Choose a Manager
+              </option>
+              {availableManagers.map((manager) => (
+                <option key={manager._id} value={manager._id}>
+                  {manager.username}
+                </option>
+              ))}
+            </Select>
+            {formData.tasks.map((task, index) => (
+              <div key={index} className="flex flex-col gap-4">
+                <Label value="task" />
+                <TextInput
+                  type="text"
+                  placeholder="task name"
+                  required
+                  value={task.name}
+                  onChange={(e) =>
+                    handleTaskChange(index, "name", e.target.value)
+                  }
                 />
+                <Label value="description" />
+                <TextInput
+                  type="text"
+                  placeholder="description"
+                  value={task.description}
+                  onChange={(e) =>
+                    handleTaskChange(index, "description", e.target.value)
+                  }
+                />
+                {/* <Label value="date" />
+                <TextInput
+                  type="date"
+                  placeholder="due date"
+                  value={task.dueDate}
+                  onChange={(e) =>
+                    handleTaskChange(index, "dueDate", e.target.value)
+                  }
+                /> */}
+
+                <Button type="button" onClick={() => removeTask(index)}>
+                  Remove Task
+                </Button>
               </div>
-            ) : (
-              'Upload Image'
-            )}
-          </Button>
+            ))}
+
+            <Button type="button" onClick={addTask}>
+              Add Task
+            </Button>
+
+            <Button type="submit" gradientDuoTone="purpleToPink">
+              Publish
+            </Button>
+
+            {publishError && <p className="text-red-500">{publishError}</p>}
+          </form>
         </div>
-        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
-        {formData.image && (
-          <img
-            src={formData.image}
-            alt='upload'
-            className='w-full h-72 object-cover'
-          />
-        )}
-        <ReactQuill
-          theme='snow'
-          value={formData.content}
-          placeholder='Write something...'
-          className='h-72 mb-12'
-          required
-          onChange={(value) => {
-            setFormData({ ...formData, content: value });
-          }}
-        />
-        <Button type='submit' gradientDuoTone='purpleToPink'>
-          Update post
-        </Button>
-        {publishError && (
-          <Alert className='mt-5' color='failure'>
-            {publishError}
-          </Alert>
-        )}
-      </form>
+      </div>
     </div>
   );
 }
