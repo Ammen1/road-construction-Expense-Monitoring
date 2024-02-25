6b17f84c6cfdf21d4492dc913e3864b1bf566e16
Ammen1 <amenguda@gmail.com>
2024-02-25 10:21:34 +0300

    update

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -143,7 +143,7 @@ export default function DashboardComp() {
           <div className="flex justify-between  p-3 text-sm font-semibold">
             <h1 className="text-center p-2">Recent posts</h1>
             <Button outline gradientDuoTone="purpleToPink">
-              <Link to={"/dashboard?tab=posts"}>See all</Link>
+              <Link to={"/dashboard?tab=projects"}>See all</Link>
             </Button>
           </div>
           <Table hoverable>
