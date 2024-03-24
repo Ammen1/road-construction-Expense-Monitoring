cda41155d316ba5a0b39d5c9be14503d2afbd524
Tamirat <amenguda@gmail.com>
2024-03-25 01:48:25 +0300

    Update sidebar navigation links and items
    This commit updates the sidebar navigation links and items based on the user's role and active tab. It introduces conditional rendering using currentUser's role (isManager, isSupplier, isFinance, isEmployee) to display specific navigation items accordingly.
    
    The following changes are made:
    - Notifications sidebar item is moved outside the isManager condition to ensure it's always displayed.
    - Tasks, Generate Report, Projects, and MyPayment sidebar items are conditionally displayed based on currentUser's role.
    - MyApplications, Apply for project, Buy, Audit, and My Tasks sidebar items are conditionally displayed based on currentUser's role.
    
    The 'active' prop is used to highlight the currently active tab in the sidebar.
    
    Closes: #4

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -13,6 +13,15 @@ import {
   HiEmojiHappy,
   HiOutlineThumbDown,
 } from "react-icons/hi";
+import {
+  MdAttachFile,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowUp,
+  MdOutlineKeyboardDoubleArrowDown,
+} from "react-icons/md";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -46,6 +55,22 @@ export default function DashSidebar() {
       console.log(error.message);
     }
   };
+
+  const getLabelBasedOnRole = () => {
+    if (currentUser.isAdmin) {
+      return "Admin";
+    } else if (currentUser.isManager) {
+      return "Manager";
+    } else if (currentUser.isSupplier) {
+      return "Supplier";
+    } else if (currentUser.isFinance) {
+      return "Finance";
+    } else if (currentUser.isEmployee) {
+      return "Employee";
+    }  else {
+      return "User";
+    }
+  };
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
@@ -65,13 +90,132 @@ export default function DashSidebar() {
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={currentUser.isAdmin ? "Admin" : "User"}
+              label={getLabelBasedOnRole()}
               labelColor="dark"
               as="div"
             >
               Profile
             </Sidebar.Item>
           </Link>
+          <Link to="/dashboard?tab=create-expense">
+            <Sidebar.Item
+              active={tab === "create-expense"}
+              icon={HiBell}
+              as="div"
+            >
+              Notifications
+            </Sidebar.Item>
+          </Link>
+          {currentUser && currentUser.isManager && (
+          <Link to="/dashboard?tab=tasks">
+          <Sidebar.Item
+            active={tab === "tasks"}
+            icon={MdAttachFile}
+            as="div"
+          >
+            Tasks
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isManager && (
+          <Link to="/dashboard?tab=projects">
+          <Sidebar.Item
+            active={tab === "projects"}
+            icon={HiDocumentText}
+            as="div"
+          >
+            Generat Report
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isManager && (
+           <Link to="/dashboard?tab=create-expense">
+           <Sidebar.Item
+             active={tab === "create-expense"}
+             icon={HiBell}
+             as="div"
+           >
+             Notifications
+           </Sidebar.Item>
+         </Link>
+          )}
+          {currentUser && currentUser.isManager && (
+          <Link to="/dashboard?tab=project">
+            <Sidebar.Item
+               active={tab === "project"}
+               icon={HiDocumentText}
+               as="div"
+             >
+               Projects
+             </Sidebar.Item>
+           </Link>
+         )}
+          {currentUser && currentUser.isSupplier && (
+          <Link to="/dashboard?tab=projects">
+          <Sidebar.Item
+            active={tab === "projects"}
+            icon={HiEmojiHappy}
+            as="div"
+          >
+            MyPayment
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isSupplier && (
+          <Link to="/dashboard?tab=myapplication">
+          <Sidebar.Item
+            active={tab === "myapplication"}
+            icon={MdKeyboardArrowDown}
+            as="div"
+          >
+            MyApplications
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isSupplier && (
+          <Link to="/dashboard?tab=approve">
+          <Sidebar.Item
+            active={tab === "approve"}
+            icon={MdOutlineKeyboardArrowUp}
+            as="div"
+          >
+            Apply for project
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isFinance && (
+          <Link to="/dashboard?tab=approve">
+          <Sidebar.Item
+            active={tab === "approve"}
+            icon={MdOutlineKeyboardDoubleArrowDown}
+            as="div"
+          >
+            Buy
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isFinance && (
+          <Link to="/dashboard?tab=audit">
+          <Sidebar.Item
+            active={tab === "audit"}
+            icon={MdOutlineKeyboardArrowUp}
+            as="div"
+          >
+            Audit
+          </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser && currentUser.isEmployee && (
+          <Link to="/dashboard?tab=task">
+          <Sidebar.Item
+            active={tab === "task"}
+            icon={MdOutlineKeyboardArrowRight}
+            as="div"
+          >
+            My Tasks
+          </Sidebar.Item>
+          </Link>
+          )}
           {currentUser.isAdmin && (
             <Link to="/dashboard?tab=projects">
               <Sidebar.Item
@@ -121,9 +265,9 @@ export default function DashSidebar() {
                   Set Schedule
                 </Sidebar.Item>
               </Link>
-              <Link to="/dashboard?tab=create-expense">
+              <Link to="/dashboard?tab=create-budget">
                 <Sidebar.Item
-                  active={tab === "create-expense"}
+                  active={tab === "create-budget"}
                   icon={HiOutlineThumbDown}
                   pill
                   as="div"
@@ -132,9 +276,9 @@ export default function DashSidebar() {
                 </Sidebar.Item>
               </Link>
 
-              <Link to="/dashboard?tab=create-expense">
+              <Link to="/dashboard?tab=approve-supplier">
                 <Sidebar.Item
-                  active={tab === "create-expense"}
+                  active={tab === "approve-supplier"}
                   icon={HiEmojiHappy}
                   as="div"
                 >
@@ -159,18 +303,10 @@ export default function DashSidebar() {
                   Approve Payment
                 </Sidebar.Item>
               </Link>
-              <Link to="/dashboard?tab=create-expense">
-                <Sidebar.Item
-                  active={tab === "create-expense"}
-                  icon={HiBell}
-                  as="div"
-                >
-                  Notifications
-                </Sidebar.Item>
-              </Link>
             </>
           )}
           <Sidebar.Item
+
             icon={HiArrowSmRight}
             className="cursor-pointer"
             onClick={handleSignout}
