2f416a3aac51869bb0409d542aa5809db83cbbca
Tamirat <amenguda@gmail.com>
2024-03-25 21:18:31 +0300

    Updated Benefits component layout to display benefits in a grid format for better readability and user experience

---
+++ client/src/components/Benefits.jsx
@@ -0,0 +1,71 @@
+import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Efficient Communication for Road Construction Teams"
+        />
+
+        <div className="grid grid-cols-1 gap-10 mb-10 md:grid-cols-3">
+          {benefits.map((item) => (
+            <div
+              className="relative bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="mb-5 h5">{item.title}</h5>
+                <p className="mb-6 body-2 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto text-xs font-bold tracking-wider uppercase font-code text-n-1">
+                    Explore more
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 transition-opacity opacity-0 hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="object-cover w-full h-full"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
